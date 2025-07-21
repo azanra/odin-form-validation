d51fbd0fc83259c6e4aba92757b1dcd3a4030e15
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-22 06:26:18 +0700

    Add form markup element

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import "./style.css";
-import img from "./asset/resource/img.png";
 
 console.log("Hello world!");
-const paraElement = document.querySelector("p");
-const imgElement = document.createElement("img");
-imgElement.src = img;
-paraElement.appendChild(imgElement);
