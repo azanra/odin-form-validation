db0ef70ed0a8de4446edd385167013f16504f06d
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-23 06:23:20 +0700

    Render error if the input is empty

--- src/validation.js
+++ src/validation.js
@@ -8,6 +8,8 @@ const validation = (function () {
       errorElement.textContent = `Entered characted should be at least ${input.minLength}, Current character are ${input.value.length}`;
     } else if (input.validity.tooLong) {
       errorElement.textContent = `Entered characted should be at least ${input.maxLength}, Current character are ${input.value.length}`;
+    } else if (input.validity.valueMissing) {
+      errorElement.textContent = "Field can't be empty";
     }
   };
 
